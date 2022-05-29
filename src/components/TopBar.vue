cf11b91304af78debe210649635b4e81dae60fd0
kromate <akpananthony33@gmail.com>
2022-05-29 21:11:59 +0100

    feat: created sections

---
+++ src/components/TopBar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex  item-center container justify-between">
+        <img src="../assets/logo.png" alt="" class="h-[63px]">
+
+        <div class="flex">
+            <span>Home</span>
+            <span>Company</span>
+            <span>Our services</span>
+            <span>Help & support</span>
+            <button class="bg-[#3B89FF] text-white py-1.5 px-3">Register</button>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
